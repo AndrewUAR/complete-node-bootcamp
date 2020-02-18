4414bec65cb81341b7fabfae2761df50197dd1b7
Andrew <andrewriznykua93@gmail.com>
2020-02-18 15:16:37 +0200

    Node-Farm project done

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate')
+
 ////////////////////////////////////////
 ////////////Files
 
@@ -28,9 +31,45 @@ const http = require('http');
 /////////////////////////////////////////////
 ///////////////Server
 
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-    console.log(req)
-    res.end('Hello from the server!');
+    const { query, pathname } = url.parse(req.url, true);
+
+    if(pathname === '/' || pathname === '/overview'){
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+
+        const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+
+        res.end(output);
+    }else if(pathname === '/product'){
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+
+    }else if(pathname === '/api'){
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+    }else{
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>This page not found</h1>')
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
